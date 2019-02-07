09c8b20dfd3df1ce870e46d1a7002ddcd2040e74
18250MariaInes <35271285+18250MariaInes@users.noreply.github.com>
2019-02-06 19:11:10 -0600

    ContactProvider agregado

--- Laboratorio5/app/src/main/java/com/example/maria/laboratorio3/agregarContacto.kt
+++ Laboratorio5/app/src/main/java/com/example/maria/laboratorio3/agregarContacto.kt
@@ -1,5 +1,6 @@
 package com.example.maria.laboratorio3
 
+import android.content.ContentValues
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+
 //clase encargada de agregar contactos al array
 class agregarContacto : AppCompatActivity() {
 
@@ -26,11 +28,17 @@ class agregarContacto : AppCompatActivity() {
 
     }//se crea un nuevo vontacto al leer los inputs del usuario
     fun agregarContacto (view: View){
-        val myapp: myapplication = applicationContext as myapplication
-        txtcorreo = findViewById(R.id.correo2)
+        //val myapp: myapplication = applicationContext as myapplication
+        val values=ContentValues()
+        txtcorreo = this.findViewById(R.id.correo2)
         txtnombre=findViewById(R.id.editNombre)
         txtnumero=findViewById(R.id.cellphone)
-        myapp.add(contact(txtnombre.text.toString(), txtnumero.text.toString(), txtcorreo.text.toString()))
+        values.put(ContactProvider.NAME,txtnombre.toString())
+        values.put(ContactProvider.EMAIL,txtcorreo.toString())
+        values.put(ContactProvider.NUMBER,txtnumero.toString())
+        val uri = contentResolver.insert(ContactProvider.CONTENT_URI, values)
+        Toast.makeText(baseContext, uri!!.toString(), Toast.LENGTH_LONG).show()
+        /*myapp.add(contact(txtnombre.text.toString(), txtnumero.text.toString(), txtcorreo.text.toString()))*/
         Toast.makeText(applicationContext, "Se ha creado nuevo contacto", Toast.LENGTH_LONG).show()
     }//boton para regresar a menu
     fun regresarmenu (view: View){
